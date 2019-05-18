f5c471cadf44d136f0c98b825e68e0bf6f85ee41
Donatas Kukta <donatas.kukta1@gmail.com>
2019-05-18 20:48:54 +0300

    Added data conversion to numer; Implemented data normalization; Created covariance matrix; Changed logic of row removals.

--- Matlab/DataPreparation.m
+++ Matlab/DataPreparation.m
@@ -7,38 +7,73 @@ DataTable = readtable('risk_factors_cervical_cancer.csv');
 QualitiveCols    = {'Smokes' 'HormonalContraceptives' 'IUD' 'STDs' 'STDs_condylomatosis' 'STDs_cervicalCondylomatosis' 'STDs_vaginalCondylomatosis' 'STDs_vulvo_perinealCondylomatosis' 'STDs_syphilis' 'STDs_pelvicInflammatoryDisease' 'STDs_genitalHerpes' 'STDs_molluscumContagiosum' 'STDs_AIDS' 'STDs_HIV' 'STDs_HepatitisB' 'STDs_HPV' 'Dx_Cancer' 'Dx_CIN' 'Dx_HPV' 'Dx'};
 QuantitativeCols = {'Age' 'NumberOfSexualPartners' 'FirstSexualIntercourse' 'NumOfPregnancies' 'Smokes_years_' 'Smokes_packs_year_' 'HormonalContraceptives_years_' 'IUD_years_' 'STDs_number_' 'STDs_NumberOfDiagnosis' 'STDs_TimeSinceFirstDiagnosis' 'STDs_TimeSinceLastDiagnosis'};
 ResultQualitiveCols = {'Hinselmann' 'Schiller' 'Citology' 'Biopsy'};
-%Age,Number of sexual partners,First sexual intercourse,Num of pregnancies,Smokes,Smokes (years),Smokes (packs/year),Hormonal Contraceptives,Hormonal Contraceptives (years),IUD,IUD (years),STDs,STDs (number),STDs:condylomatosis,STDs:cervical condylomatosis,STDs:vaginal condylomatosis,STDs:vulvo-perineal condylomatosis,STDs:syphilis,STDs:pelvic inflammatory disease,STDs:genital herpes,STDs:molluscum contagiosum,STDs:AIDS,STDs:HIV,STDs:Hepatitis B,STDs:HPV,STDs: Number of diagnosis,STDs: Time since first diagnosis,STDs: Time since last diagnosis,Dx:Cancer,Dx:CIN,Dx:HPV,Dx,Hinselmann,Schiller,Citology,Biopsy
 
-
+% Pasalinti stulpelius, kurie turi per daug trukstanciu reiksmiu
 missingDataCounts = getMissingValCountByCol(DataTable, size(ResultQualitiveCols,2));
 varsToBeRemoved = identifyVariablesToBeDropped(missingDataCounts, height(DataTable), 0.5) ;
 [DataTable, QuantitativeCols,QualitiveCols] = removeVariables(DataTable, varsToBeRemoved, QuantitativeCols,QualitiveCols);
-%DataTable = removeRows(DataTable(:,DataTable.Properties.VariableNames([QualitiveCols,QuantitativeCols])), 0.5)
-DataTable = removeRows(DataTable, 0.5,size(ResultQualitiveCols,2));
-DataTable = processQuantativeCols(DataTable,QuantitativeCols );
-colOne =  DataTable{:,1};
-colTwo =  str2double(DataTable{:,3});
-class(colOne)
-class(colTwo)
-scatter(colOne,colTwo)
-% Ka daryti su trukstamais kokybiniais duomenimis?
 
-% Isvalius ir paruosus duomenis atlikti ekspermentus dimensiju mazinimui:
-% Kintamuju tarpusavio priklausomybiu palyginimas;
-% ...
-% Ka reiskia formules atributai?
-function result = normalize(x, averageX, maxX, minX)
-    result = (x - averageX) / (maxX - minx);
+% Pasalinti eilutes, kurios turi atitinkamai trukstamu reiksmiu
+DataTable = removeRows(DataTable, 0.5,size(ResultQualitiveCols,2), QualitiveCols);
+
+% Apdoroti stulpelius, kurie turi kiekybines reiksmes 
+%   (pasalinti nuokrypius ir nezinomas reiksmes pakeisti vidurkiu).
+DataTable = processQuantativeCols(DataTable,QuantitativeCols,3 );
+
+% Paversti visas reiksmes i skaicius bei juos normalizuoti.
+DataTable = convertToNormalizedValues(DataTable,QuantitativeCols);
+
+% Sudaryti kovariaciju matrica
+matrixData = table2array(DataTable);
+cov(matrixData)
+
+writetable(DataTable, 'results.csv')
+%scatter(colOne,colTwo)
+%histogram(DataTable{:,30});
+
+function newTable = convertToNormalizedValues(oldTable, quanC)
+newTable = array2table(zeros(height(oldTable),width(oldTable)));
+newTable.Properties.VariableNames = oldTable.Properties.VariableNames;
+allnames = newTable.Properties.VariableNames;
+for i=1:width(oldTable)
+    currCol = oldTable{:,i};
+    colType = convertCharsToStrings(class(currCol));
+    if colType ~= "double"
+        %currCol = str2double(string(currCol));
+        stringCol = string(currCol);
+        currCol = str2double(stringCol);
+        %oldTable{:,i} = currCol
+    end
+    % Jei tai kiekybinis stulpelis- normalizuojame
+    [isQuantative, index] = ismember(allnames(1,i), quanC);
+
+    if(isQuantative && isDiffElements(currCol))
+        currCol = normalize(currCol);
+    end
+    newTable{:,i} = currCol;    
 end
-%Atlikti normalizacija?
-%Jei taip, ar metode esanti normalize() yra gera formule?
+end
 
-% ---------------------Metodai: ---------------------
+function result = normalize(array)
+    minV = min(array);
+    maxV = max(array);
+    meanV = mean(array);
+    result = (array - minV) / (maxV - minV);
+end
 
-% Apdoroti kiekybinius stulpelius:
-%   suteikti vidutines reiksmes trukstamiem stulpeliams
-%   pasalinti nuokrypius
-function table = processQuantativeCols(table, quantitativeCols)
+function rez = isDiffElements(array)
+    n = size(array,1) - 1;
+    for i=1:n
+        if (array(i) ~= array(i+1))
+            rez = 1;
+           return;
+        end
+    end
+    rez = 0;
+    return
+end
+
+function table = processQuantativeCols(table, quantitativeCols,SDCoefficient)
     qColCount = size(quantitativeCols,2);
     names = table.Properties.VariableNames;
     for i=1:qColCount
@@ -54,15 +89,17 @@ function table = processQuantativeCols(table, quantitativeCols)
        %    Kaip tiksliai nustatyti ar reiksme nukrypusi?
        length = height(table);
        j = 1;
+       minDev = mean - (SDCoefficient * sd );
+       maxDev = mean + (SDCoefficient * sd );       
        while j <= length
-          currentValue = string(currentCol(j,1));
+          currentValue = string(table{:,currentNameIndex}(j,1));
            if isequal( currentValue , symbol)
                %Jei reiksme nezinoma, priskirti vidurki
                table{j,currentNameIndex} = cellstr(string(mean));
            else
                % Jei reiksme zinoma, ziureti, ar ji ne per daug nukrypusi
-               isDeviated = 0;
-               if isDeviated
+               currNumVal = str2double(currentValue);
+               if currNumVal < minDev || maxDev < currNumVal
                    %Jei nuokrypis didelis, salinti visa eilute
                    table(j,:) = [];
                    length = length - 1;
@@ -72,7 +109,6 @@ function table = processQuantativeCols(table, quantitativeCols)
           j = j + 1;
        end
     end
-    result = 0;
 end
 % Gauti standartini nuokrypi
 function sd =getStandartDeviation(array, mean)
@@ -107,7 +143,7 @@ function mean =getMean(array)
 end
 
 % Pasalina eilutes kurios neturi pakankamai daug duomenu
-function table = removeRows(table, missingDataThreshold, resultCount)
+function table = removeRows(table, missingDataThreshold, resultCount, qualCols)
     n = height(table);
     rowCount = (width(table) - resultCount);
     i=1;
@@ -115,7 +151,8 @@ function table = removeRows(table, missingDataThreshold, resultCount)
     row = table(i,:);
     missingCount = getMissingCountinRow(row);
     missingRatio = missingCount / rowCount;
-    if missingRatio >= missingDataThreshold
+    hasMissingQualVal = hasMissingValue(row, qualCols);
+    if missingRatio >= missingDataThreshold || hasMissingQualVal
         %pasalinti eilute...
         table(i,:) = [];
         i= i-1;
@@ -123,6 +160,29 @@ function table = removeRows(table, missingDataThreshold, resultCount)
     end
     i = i + 1;
     end  
+end
+
+function rez = hasMissingValue(tableRow, Cols)
+    colN = width(tableRow);
+    allNames = tableRow.Properties.VariableNames;
+    symbol = "?";
+    for col = 1:colN
+        [isInCols, index] = ismember(allNames(1,col), Cols);
+        %Jei stulpelis yra tarp nurodyru stulpeliu
+        if isInCols
+            val = string(tableRow{1,col}(1,1));
+            %Jei kintamojo reiksme yra nezinoma
+            if isequal( val,symbol)
+                % return true
+                rez = 1;
+                return;
+            else
+            end
+        end
+    end
+    %return false
+    rez = 0;
+    return;
 end
 
 function count = getMissingCountinRow(row)
@@ -147,7 +207,6 @@ function [newTable, quanC, qualC] = removeVariables(table, vars, quanC, qualC)
         quanC = setdiff(quanC, name);
         qualC = setdiff(qualC, name);
     end
-    
 end
 
 
@@ -167,7 +226,6 @@ function errorDataCount = getMissingValCountByCol(table, resultCount)
     rowN = height(table);
     errorDataCount = zeros(1,colN);
     symbol = "?";
-    %      table{eilute, stulpelis};
     for col = 1:colN
         for row = 1 : rowN
             val = string(table{row,col}(1,1));
